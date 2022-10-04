3d9f6d7f0eb63779ff8a4197570f6131edd98b1c
reizergrzegorz <reizergrzegorz@gmail.com>
2022-10-04 11:27:55 +0200

    add headers for user list

--- util.py
+++ util.py
@@ -2,6 +2,7 @@ from datetime import datetime
 
 ANSWER_HEADER = ['submission_time', 'vote_number', 'question_id', 'message']
 QUESTION_HEADER = ['title', 'message','view number', 'title' , 'message']
+USER_HEADER = ['login', 'register_time']
 
 def get_time():
     return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
