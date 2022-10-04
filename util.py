4d088b5ac7d1ca378122474fdfa2013a70f1ac54
reizergrzegorz <reizergrzegorz@gmail.com>
2022-10-04 13:35:49 +0200

    users list headers update

--- util.py
+++ util.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 ANSWER_HEADER = ['submission_time', 'vote_number', 'question_id', 'message']
 QUESTION_HEADER = ['title', 'message','view number', 'title' , 'message']
-USER_HEADER = ['login', 'register_time']
+USER_HEADER = ['login', 'registration_date']
 
 def get_time():
     return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
